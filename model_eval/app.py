3131f2f53097b3857e0e1a490883d52e418e2de7
reconlabs-luke <luke@reconlabs.ai>
2023-10-18 19:14:13 +0900

    init setup

---
+++ model_eval/app.py
@@ -0,0 +1,9 @@
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    print("model_eval")
+
+    args = None
+    main(args)
---
+++ model_train/app.py
@@ -0,0 +1,8 @@
+def main(args):
+    pass
+
+
+if __name__ == "__main__":
+    print("model_train")
+    args = None
+    main(args)
