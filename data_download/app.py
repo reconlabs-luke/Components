8ff4715348b841b8b089e95e5eb8ae67863f09f7
reconlabs-luke <luke@reconlabs.ai>
2023-10-19 19:00:06 +0900

    data loader component

--- data_download/app.py
+++ data_download/app.py
@@ -1,20 +1,32 @@
 import argparse
+from torchvision import datasets, transforms
 
 
 def main(args):
-    print(args)
+    if args.path == "MNIST":
+        datasets.MNIST(
+            root=args.output_path,  # 다운로드 경로 지정
+            train=True,  # False를 지정하면 테스트 데이터로 다운로드
+            transform=transforms.Compose(
+                [
+                    transforms.ToTensor(),
+                    transforms.Normalize(mean=(0.5,), std=(0.5,)),
+                ]
+            ),
+            download=True,
+        )
 
 
 if __name__ == "__main__":
-    print("data download")
     parser = argparse.ArgumentParser()
     parser.add_argument(
         "-p",
         "--path",
         dest="path",
-        default="./",
+        default="MNIST",
         action="store",
     )
+
     parser.add_argument(
         "-o",
         "--output_path",
